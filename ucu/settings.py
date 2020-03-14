1be852ceadad77e367087080f7638a38c0d4726c
jtertil <jtertil@gmail.com>
2020-03-14 02:56:31 +0100

    debug [WIP] change db to postgrs

--- ucu/settings.py
+++ ucu/settings.py
@@ -90,10 +90,14 @@ WSGI_APPLICATION = 'ucu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_var('DATABASE_NAME'),
+        'USER': get_env_var('DATABASE_USER'),
+        'PASSWORD': get_env_var('DATABASE_PASSWORD'),
+        'HOST': get_env_var('DATABASE_HOST'),
+        'PORT': get_env_var('DATABASE_PORT'),
+        },
     }
-}
 
 
 # Password validation
