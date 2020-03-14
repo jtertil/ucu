3aae9601b61af6e08babfd99523fbc43b2541f7b
jtertil <jtertil@gmail.com>
2020-03-14 03:14:03 +0100

    readme, turn off debug, randomize base

--- webapp/helpers.py
+++ webapp/helpers.py
@@ -3,5 +3,5 @@ from django.utils.baseconv import BaseConverter
 # symbols allowed in URL's:
 # '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._~'
 
-BASE_SYMBOLS = 'abcdefghijklmnopqrstuvwxyz0123456789'
+BASE_SYMBOLS = 'oxr2c7njdhay4bi51wms3t0p6vfkqzle9gu8'
 converter = BaseConverter(BASE_SYMBOLS)
