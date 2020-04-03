5d043d9628e070347513df279aea4d383984bbf0
jtertil <jtertil@gmail.com>
2020-04-03 13:27:52 +0200

    dev update

--- ucu/settings.py
+++ ucu/settings.py
@@ -35,7 +35,9 @@ SECRET_KEY = get_env_var('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
